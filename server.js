485683a9d1d22a61f9bb4a37b3418c697741db98
aadu999 <adarshr@live.in>
2023-03-26 14:51:34 +0530

    added filesend

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log('test')
+const express = require("express")
+const app = express()
+const port = 3000
+const path = require("path")
+const bodyParser = require("body-parser")
+
+app.use(express.static(path.join(__dirname, "public")))
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1><a href='/page'>Go to Page</a>")
+})
+app.get("/page", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"))
+})
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.post("/login", (req, res) => {
+  // log the request from req
+  console.log(req.body)
+  res.send("You are logged in")
+})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
